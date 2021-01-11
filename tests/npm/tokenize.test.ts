28d305d339c1b7b93f16e5522c9348beb77684b6
Mohamed taha Baz <mobaz@e1r4p11.1337.ma>
2021-01-11 12:24:46 +0100

    add more uunit tests to the nom tokenizer

--- tests/npm/tokenize.test.ts
+++ tests/npm/tokenize.test.ts
@@ -3,7 +3,6 @@ import IToken, {Tokens} from '../../Lexers/tokens'
 import tokenize, {newToken} from 'build/../../Lexers/npm/tokenize'
 
 
-
 it("tests the tokenizer with basic input", () => {
 	const tok: IToken<npmType, npmVal>[] = [
 		{
@@ -46,4 +45,23 @@ it("tests the tokenizer with different input", () => {
 		}
 	];
 	expect(tokenize("npm i jest", npmTokens)).toEqual(tok)
+	const tok2: IToken<npmType, npmVal>[] = [
+		newToken("NPM", "npm"),
+		newToken("COMMAND", "access"),
+		newToken("ARGUMENT", "grant")
+	]
+	expect(tokenize("npm access grant", npmTokens)).toEqual(tok2)
+	const tok3 : IToken<npmType, npmVal>[]= [
+		newToken("NPM", "npm"),
+		newToken("COMMAND", "access"),
+		newToken("ARGUMENT", "revoke")
+	]
+	expect(tokenize("npm access revoke", npmTokens)).toEqual(tok3)
+	const tok4 : IToken<npmType, npmVal>[]= [
+		newToken("NPM", "npm"),
+		newToken("COMMAND", "add"),
+		newToken("UNKNOWN", "some"),
+		newToken("UNKNOWN", "package")
+	]
+	expect(tokenize("npm add some package", npmTokens)).toEqual(tok4)
 })
