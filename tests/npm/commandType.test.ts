2ef2366eebea7ab2cd0dfdce8a2bc1193bd669a0
Mohamed taha Baz <mobaz@e1r5p2.1337.ma>
2021-01-14 18:14:22 +0100

    mock the mongoose create function

--- tests/npm/commandType.test.ts
+++ tests/npm/commandType.test.ts
@@ -3,6 +3,7 @@ import connect from '../../db/connect'
 import commandType, {IMinCommandType} from '../../models/CommandType'
 import commandTypeService from '../../services/commandTypeService'
 import { IMinCommand } from '../../models/Command'
+import CommandType from '../../models/CommandType'
 
 afterEach(async () => {
 	await commandType.deleteMany()
@@ -17,12 +18,15 @@ beforeAll(async () => {
 })
 
 describe('Create a Command Type', () => {
-	it('should add a command type',  () => {
+	it('should add a command type',  async (done) => {
+		expect.assertions(1)
 		const commandType: IMinCommandType = {
 			name: 'npm',
 			description: 'node package manager',
 		}
-		return commandTypeService.createOne(commandType)
-					.then(({name, description}) => expect({name, description}).toEqual(commandType))
+		jest.spyOn(CommandType, 'create').mockReturnValue(Promise.resolve(commandType))
+		const {name, description} = await commandTypeService.createOne(commandType)
+		expect({name, description}).toEqual(commandType)
+		done()
 	})
 })
