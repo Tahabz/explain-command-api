603c3d4348b1a7726abc4405e108711f6ec4cc92
Mohamed taha Baz <mobaz@e1r5p2.1337.ma>
2021-01-16 18:15:40 +0100

    add get unit test to argument

--- tests/npm/argument.test.ts
+++ tests/npm/argument.test.ts
@@ -1,23 +1,23 @@
 import connect from '../../db/connect'
-import Argument, {IMinArgument} from '../../models/Argument'
+import Argument, { IMinArgument } from '../../models/Argument'
 import ArgumentService from '../../services/argumentService'
 import mongoose from 'mongoose'
 
 afterEach(async () => {
- await Argument.deleteMany()
+	await Argument.deleteMany()
 })
 
 beforeAll(async () => {
 	try {
 		await connect()
-	} catch(e) {
+	} catch (e) {
 		console.log(e);
 	}
 })
 
 
 describe('Create an Argument', () => {
-	it('should add an Argument',  async (done) => {
+	it('should add an Argument', async (done) => {
 		expect.assertions(3)
 
 		const restricted: IMinArgument = {
@@ -60,14 +60,51 @@ describe('update an Argument', () => {
 		}
 		const argId = mongoose.Types.ObjectId()
 		await ArgumentService.createOne(restricted)
-		
+
 		const updated = await ArgumentService.updateOne(
-			{name: 'restricted'},
-			{description: 'allowed package from npm'},
-			{new: true}
+			{ name: 'restricted' },
+			{ description: 'allowed package from npm' },
+			{ new: true }
 		)
 		expect(updated._id).toBeDefined()
 		expect(updated.description).toBe('allowed package from npm')
+		done()
+	})
+})
+
+describe('get command', () => {
+	it('should get all commands', async (done) => {
+		expect.assertions(1)
+		const restricted: IMinArgument = {
+			name: 'restricted',
+			description: 'restricted package from npm',
+		}
+		const allowed: IMinArgument = {
+			name: 'allowed',
+			description: 'alow package from npm',
+		}
+		await ArgumentService.createOne(restricted)
+		await ArgumentService.createOne(allowed)
+
+		const commands = await ArgumentService.getAll()
+
+		expect(commands.length).toBeGreaterThan(0)
+		done()
+	})
+
+	it('should get a specific command', async (done) => {
+		expect.assertions(3)
+
+		const allowed: IMinArgument = {
+			name: 'allowed',
+			description: 'alow package from npm',
+		}
+		await ArgumentService.createOne(allowed)
+
+		const getallowed = await ArgumentService.getOne({ name: allowed.name })
+		expect(getallowed._id).toBeDefined()
+		expect(getallowed.name).toBe(allowed.name)
+		expect(getallowed.description).toBe(allowed.description)
 		done()
 	})
 })
